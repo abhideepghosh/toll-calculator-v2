a1c47f42cc7a477d91c818edb95c6f43da69349c
gabhiaf98 <gabhiaf98@gmail.com>
2023-12-10 09:55:23 +0000

    leaflet

--- src/components/display/Display.jsx
+++ src/components/display/Display.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import "./Display.css";
 
 const Display = () => {
   const [markers, setMarkers] = useState([]);
 
   useEffect(() => {
     if (markers.length === 2) {
+      // Log coordinates when both markers are placed
       console.log(`From: ${markers[0].lat}, ${markers[0].lng}`);
       console.log(`To: ${markers[1].lat}, ${markers[1].lng}`);
     }
@@ -15,8 +15,10 @@ const Display = () => {
 
   const handleMarkerClick = (event) => {
     if (markers.length < 2) {
+      // Add marker only if less than 2 exist
       setMarkers((prevMarkers) => [...prevMarkers, event.latlng]);
     } else {
+      // Update only the second marker if 2 exist
       setMarkers((prevMarkers) => [
         ...prevMarkers.slice(0, 1), // Keep first marker
         event.latlng,
@@ -24,13 +26,17 @@ const Display = () => {
     }
   };
 
+  const handleMapClick = () => {
+    setMarkers([]);
+  };
+
   return (
     <MapContainer
       center={[22.531404, 88.365632]}
       zoom={13}
       scrollWheelZoom={true}
-      style={{ height: "70vh", width: "100%" }}
-      onClick={handleMarkerClick}
+      style={{ height: "400px", width: "100%" }}
+      onClick={handleMapClick}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
