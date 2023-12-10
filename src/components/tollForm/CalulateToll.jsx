1fd1dd6ca14d95989e43764cdd6ce622c01e7f63
gabhiaf98 <gabhiaf98@gmail.com>
2023-12-10 12:48:51 +0000

    Cors Issue

--- src/components/tollForm/CalulateToll.jsx
+++ src/components/tollForm/CalulateToll.jsx
@@ -3,44 +3,60 @@ import "./CalculateToll.css";
 const CalculateToll = ({ fromTo, resetFromTo }) => {
   console.log(fromTo);
 
-  const generateToll = () => {
-    const data = fetch(
-      "https://apis.tollguru.com/toll/v2/origin-destination-waypoints",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "x-api-key": "jfNQjLR6nj2pLPPbQB6j32hrBPG4PrN9",
+  const generateToll = async () => {
+    try {
+      const url =
+        "https://apis.tollguru.com/toll/v2/origin-destination-waypoints";
+      const apiKey = "jfNQjLR6nj2pLPPbQB6j32hrBPG4PrN9";
+      const body = JSON.stringify({
+        from: {
+          lat: 39.95209,
+          lng: -75.16219,
         },
-        body: JSON.stringify({
-          from: {
-            // address: 'Philadelphia , Pennsylvania',
-            lat: 39.95209,
-            lng: -75.16219,
+        to: {
+          lat: 40.71455,
+          lng: -74.00715,
+        },
+        waypoints: [
+          {
+            address: "Bridgewater Township , New Jersey",
           },
-          to: {
-            // address: 'New York ,NY',
-            lat: 40.71455,
-            lng: -74.00715,
+        ],
+        serviceProvider: "here",
+        vehicle: {
+          type: "2AxlesTaxi",
+          weight: {
+            value: 20000,
+            unit: "pound",
           },
-          // waypoints: [{ address: 'Bridgewater Township , New Jersey' }],
-          serviceProvider: "here",
-          vehicle: {
-            type: "2AxlesTaxi",
-            weight: { value: 20000, unit: "pound" },
-            height: { value: 7.5, unit: "meter" },
-            length: { value: 7.5, unit: "meter" },
-            axles: 4,
-            emissionClass: "euro_5",
+          height: {
+            value: 7.5,
+            unit: "meter",
           },
-        }),
-      }
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => {
-        console.error(error);
+          length: {
+            value: 7.5,
+            unit: "meter",
+          },
+          axles: 4,
+          emissionClass: "euro_5",
+        },
       });
+      const headers = {
+        "Content-Type": "application/json",
+        "x-api-key": apiKey,
+      };
+
+      const options = {
+        method: "POST",
+        headers,
+        body,
+      };
+      const response = await fetch(url, options);
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
