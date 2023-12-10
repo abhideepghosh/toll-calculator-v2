0521a05d4ea16086745a8bdbb49aa0599a862c1a
gabhiaf98 <gabhiaf98@gmail.com>
2023-12-10 09:38:22 +0000

    Header Implementation - Begin

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Display from "./components/display/Display";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Display />
     </div>
   );
