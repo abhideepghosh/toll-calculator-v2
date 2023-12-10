03646474334e98e5c749d622b9888a92eef3101f
gabhiaf98 <gabhiaf98@gmail.com>
2023-12-10 09:39:02 +0000

    Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
-  return <div className="header">Header</div>;
+  return <div className="header">Toll Calculator</div>;
 };
 
 export default Header;
